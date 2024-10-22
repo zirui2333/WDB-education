dd882111fa12a175a9aff9eadac9efe697abd854
zirui2333 <zhengzirui43@gmail.com>
2024-10-21 21:34:49 -0400

    new update cal

--- calculator_js.js
+++ calculator_js.js
@@ -6,7 +6,22 @@ document.getElementById('add_row').addEventListener('click',function(){
     row.innerHTML = `
         <td><input type="checkbox" class="GPA_input check_mark" unchecked></td>
         <td><input type="text" class="GPA_input" placeholder="Course"></td>
-        <td><input type="text" class="GPA_input grade_input" placeholder="--"></td>
+        <td>
+                    <select class="GPA_input grade_input">
+                        <option value="" selected disabled>--</option>
+                        <option value="A+">A+</option>
+                        <option value="A">A</option>
+                        <option value="A-">A-</option>
+                        <option value="B+">B+</option>
+                        <option value="B">B</option>
+                        <option value="B-">B-</option>
+                        <option value="C+">C+</option>
+                        <option value="C">C</option>
+                        <option value="C-">C-</option>
+                        <option value="D">D</option>
+                        <option value="F">F</option>
+                    </select>
+                </td>
         <td><input type="number" class="GPA_input credit_input"></td>
         <td><button class="GPA_input delete">&times;</button></td>
     `;
@@ -22,11 +37,15 @@ document.getElementById('course_table').addEventListener('click', function(event
 
 
 const letter_grade_points = {
+    "A+": 4.0,
     "A" : 4.0,
+    "A-": 3.7,
     "B+": 3.5,
     "B" : 3.0,
+    "B-": 2.7,
     "C+": 2.5,
     "C" : 2.0,
+    "C-": 1.7,
     "D" : 1.0,
     "F" : 0.0
 };
@@ -47,7 +66,7 @@ document.getElementById('calculate').addEventListener('click', function(){
         const mark = marks[i].checked;
         
         if(mark){
-            if (!letter_grade_points[grade] || isNaN(credit) || credit < 0) {
+            if (isNaN(credit) || credit < 0) {
                 GPA_board.innerHTML = `
                     <p>
                         Wrong format!<br>
@@ -73,4 +92,14 @@ document.getElementById('reset').addEventListener('click', function(){
     location.reload();
 });
 
+document.getElementById('credit_input').addEventListener('input', function(event){
+    const credit = event.target.value;
 
+    const last_digit = credit % 10;
+    
+    if(last_digit < 0 || last_digit > 5){
+        event.target.value = Math.round(credit / 10);
+    }else{
+        event.target.value = last_digit;
+    }
+});
